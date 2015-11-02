2bcfd558f51cd1a505076d29befd1737076f3191
ViscountPudding <stephenmccammon@comcast.net>
2015-11-02 11:58:12 -0700

    Some Unit Test things

--- java/src/client/tests/ModelFacadeUnitTests.java
+++ java/src/client/tests/ModelFacadeUnitTests.java
@@ -8,14 +8,19 @@ import java.util.List;
 import org.junit.Test;
 
 import shared.definitions.CatanColor;
+import client.model.DevCardList;
+import client.model.MessageList;
 import client.model.Player;
+import client.model.ResourceList;
+import client.model.TransferMap;
 import client.model.TransferModel;
+import client.model.TurnTracker;
 
 public class ModelFacadeUnitTests {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		//fail("Not yet implemented");
 		
 		TransferModel HeadBrick = new TransferModel();
 	
@@ -26,6 +31,20 @@ public class ModelFacadeUnitTests {
 		brickPlayers.add(new Player("Ka", 2, CatanColor.RED, 789));
 		brickPlayers.add(new Player("Zan", 3, CatanColor.PURPLE, 000));
 		
+		HeadBrick.setPlayers(brickPlayers);
+		
+		HeadBrick.setDeck(new DevCardList(2, 5, 2, 14, 2));
+	
+		HeadBrick.setBank(new ResourceList(19,19,19,19,19));
+		
+		HeadBrick.setChat(new MessageList());
+		HeadBrick.setLog(new MessageList());
+		
+		HeadBrick.setTurnTracker(new TurnTracker(0,0,5,3));
+		
+		TransferMap BrickMap = new TransferMap();
+		
+		HeadBrick.setVersion(0);
 	}
 
 }
