684ff7e83f9dffd96f44a2cf8aa8129942ec3439
ViscountPudding <stephenmccammon@comcast.net>
2015-11-09 12:38:06 -0700

    MADE THE MOVES METHODS

---
+++ java/src/server/facades/ServerMovesFacade.java
@@ -0,0 +1,73 @@
+package server.facades;
+
+import shared.transferClasses.AcceptTrade;
+import shared.transferClasses.BuildCity;
+import shared.transferClasses.BuildRoad;
+import shared.transferClasses.BuildSettlement;
+import shared.transferClasses.BuyDevCard;
+import shared.transferClasses.DiscardCards;
+import shared.transferClasses.FinishTurn;
+import shared.transferClasses.MaritimeTrade;
+import shared.transferClasses.Monopoly;
+import shared.transferClasses.Monument;
+import shared.transferClasses.OfferTrade;
+import shared.transferClasses.RoadBuilding;
+import shared.transferClasses.RobPlayer;
+import shared.transferClasses.RollNumber;
+import shared.transferClasses.SendChat;
+import shared.transferClasses.Soldier;
+import shared.transferClasses.YearOfPlenty;
+
+public class ServerMovesFacade {
+
+	public void acceptTrade(int gameID, AcceptTrade accept) {
+	}
+	
+	public void buildCity(int gameID, BuildCity build) {
+	}
+	
+	public void buildRoad(int gameID, BuildRoad build) {
+	}
+	
+	public void buildSettlement(int gameID, BuildSettlement build) {
+	}
+	
+	public void buyDevCard(int gameID, BuyDevCard buy) {	
+	}
+	
+	public void discardCards(int gameID, DiscardCards discard) {
+	}
+	
+	public void finishTurn(int gameID, FinishTurn end) {		
+	}
+	
+	public void maritimeTrade(int gameID, MaritimeTrade trade) {		
+	}
+	
+	public void monopoly(int gameID, Monopoly monoploy) {		
+	}
+	
+	public void monument(int gameID, Monument monument) {		
+	}
+	
+	public void offerTrade(int gameID, OfferTrade offer) {		
+	}
+	
+	public void roadBuilding(int gameID, RoadBuilding roadBuild) {		
+	}
+	
+	public void robPlayer(int gameID, RobPlayer robbery) {		
+	}
+	
+	public void rollNumber(int gameID, RollNumber roll) {		
+	}
+	
+	public void sendChat(int gameID, SendChat chat) {	
+	}
+	
+	public void soldier(int gameID, Soldier soldier) {		
+	}
+	
+	public void yearOfPlenty(int gameID, YearOfPlenty plenty) {		
+	}
+}
